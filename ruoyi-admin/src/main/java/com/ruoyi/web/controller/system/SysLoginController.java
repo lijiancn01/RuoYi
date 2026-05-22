23f5934dc78de7774a2f36098948df3824c4a401
lijiancn01 <366235974@qq.com>
2026-05-22 06:56:14 +0000

    feat: 前端技术栈替换
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginController.java
@@ -52,6 +52,21 @@ public class SysLoginController extends BaseController
         return "login";
     }
 
+    @GetMapping("/login-new")
+    public String loginNew(HttpServletRequest request, HttpServletResponse response, ModelMap mmap)
+    {
+        // 如果是Ajax请求，返回Json字符串。
+        if (ServletUtils.isAjaxRequest(request))
+        {
+            return ServletUtils.renderString(response, "{\"code\":\"1\",\"msg\":\"未登录或登录超时。请重新登录\"}");
+        }
+        // 是否开启记住我
+        mmap.put("isRemembered", rememberMe);
+        // 是否开启用户注册
+        mmap.put("isAllowRegister", Convert.toBool(configService.getKey("sys.account.registerUser"), false));
+        return "login-new";
+    }
+
     @PostMapping("/login")
     @ResponseBody
     public AjaxResult ajaxLogin(String username, String password, Boolean rememberMe)
